a960d70909b169fde9d9b3e79f1b794933ae4f63
Mineko <2713889+mineko@users.noreply.github.com>
2025-09-01 14:10:06 -0700

    Initial Commit

---
+++ HTMLConsoleApp/HTMLConsoleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HTMLConsoleAppApp.swift
+//  HTMLConsoleApp
+//
+//  Created by Collin Pieper on 9/1/25.
+//
+
+import SwiftUI
+
+@main
+struct HTMLConsoleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HTMLConsoleAppTests/HTMLConsoleAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  HTMLConsoleAppTests.swift
+//  HTMLConsoleAppTests
+//
+//  Created by Collin Pieper on 9/1/25.
+//
+
+import Testing
+@testable import HTMLConsoleApp
+
+struct HTMLConsoleAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
